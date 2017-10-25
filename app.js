96e8214b702731bf59ade3ed30b30f549230f223
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-10-25 20:39:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//api key AIzaSyAn_Yo9LabkG3IOTNWT30u_NiHkjUa2nHk
+
+// const shortenUrl = {
+//     method: 'POST',
+//     headers: new Headers({
+//         'Content-Type': 'application/json'
+//     }),
+//     body: JSON.stringify({
+//         longUrl: document.querySelector('.url-input').value
+//     })
+// }
+
+//Expand
+
+document.getElementById('expand').addEventListener
+('click', expandUrl)
+
+
+function expandUrl(){
+    const textUrl = document.getElementById("urlInput").value
+    console.log(textUrl)
+
+    fetch(`https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyAn_Yo9LabkG3IOTNWT30u_NiHkjUa2nHk&shortUrl=${textUrl}`)
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson))
+}
+
