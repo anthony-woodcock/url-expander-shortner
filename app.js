b523a7d4c71ca56fafeadf6c0c67c8cb74e8eebc
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-10-29 13:28:37 +0000

    expand and shorten connect with the DOM

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 //api key AIzaSyAn_Yo9LabkG3IOTNWT30u_NiHkjUa2nHk
 
-// const shortenUrl = {
-//     method: 'POST',
-//     headers: new Headers({
-//         'Content-Type': 'application/json'
-//     }),
-//     body: JSON.stringify({
-//         longUrl: document.querySelector('.url-input').value
-//     })
-// }
-
 //Expand
 
 document.getElementById('expand').addEventListener
@@ -29,5 +19,33 @@ function expandUrl(){
         })
         .then(responseJson => document.getElementById("urlInput").value = innerHTML = responseJson.longUrl)
         
+}
+
+//Shorten
+
+document.getElementById('shorten').addEventListener
+('click', shortenUrl)
+
+function shortenUrl(){
+    const textUrl = document.getElementById("urlInput").value
+    
+    fetch(`https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyAn_Yo9LabkG3IOTNWT30u_NiHkjUa2nHk&shortUrl=${textUrl}`, {
+        method: 'POST',
+        headers: {
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            longUrl: textUrl
+        })
+    })
+    .then(response => {
+        if(response.status !== 200) {
+            throw new Error('Response status not 200, please enter a valid link')
+        }
+
+        return response.json()
+    })
+    .then((responseJson) => document.getElementById("urlInput").value = innerHTML = responseJson.id)
+
 }
 
